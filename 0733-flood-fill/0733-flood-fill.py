b8dad5467cd53774f84b562242e56ed2cc3ba0c0
sbtiffanykim <sbtiffanykim@gmail.com>
2025-10-16 23:39:42 +0900

    Time: 4 ms (6.59%), Space: 18.2 MB (26.7%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,20 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        m, n = len(image), len(image[0])       
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        starting_color = image[sr][sc]
+
+        queue = deque()
+        queue.append((sr, sc))
+        visited[sr][sc] = True
+        image[sr][sc] = color
+
+        while queue:
+            x, y = queue.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                # skip cells that are out of bounds, visited, or not the starting color
+                if nx < 0 or nx >= m or ny < 0 or ny >= n or visited[nx][ny] or image[nx][ny] != starting_color:
+                    continue
+                visited[nx][ny] = True
+                image[nx][ny] = color
+                queue.append((nx, ny))
         
-        n, m = len(image), len(image[0])
-        
-        def dfs(x, y):
-            if x < 0 or x >= n or y < 0 or y >= m:
-                return image
-            if image[x][y] == target:
-                image[x][y] = color
-                dfs(x - 1, y)
-                dfs(x + 1, y)
-                dfs(x, y - 1)
-                dfs(x, y + 1)
-            return image
-        
-        target = image[sr][sc]
-        if target == color:
-            return image
-        return dfs(sr, sc)
+        return image
