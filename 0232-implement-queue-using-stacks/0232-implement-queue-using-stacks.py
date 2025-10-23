9d59fefd2e3be4863585e62c29d2362ab2a2ef61
sbtiffanykim <sbtiffanykim@gmail.com>
2025-10-23 23:11:19 +0900

    Time: 0 ms (100%), Space: 18 MB (21.01%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,29 @@
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.out_stack.pop()  # Pops from the stack to remove the leftmost element from the queue
+
+    def peek(self) -> int:
+        if not self.out_stack:  # If out_stack is empty, transfer elements from in_stack
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.in_stack and not self.out_stack
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
