532db96c9d7756de5487377e27da4e601fd80309
sbtiffanykim <sbtiffanykim@gmail.com>
2025-11-06 14:43:17 +0900

    Time: 3 ms (40.56%), Space: 18 MB (7.23%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        a = Counter(s)
+        odd_found = False
+        length = 0
+
+        for v in Counter(s).values():
+            if v % 2 == 0:
+                length += v  # Use all characters if count is even
+            else:
+                length += v - 1  # Use all but one character to keep it even
+                odd_found = True
+        
+        if odd_found:
+            length += 1  # One odd character can be placed in the center
+        
+        return length
