5022cb238fcf489801539046e16aec7ed1c0dd57
sbtiffanykim <sbtiffanykim@gmail.com>
2026-01-01 23:42:48 +0900

    Time: 395 ms (96.75%), Space: 20 MB (97.5%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        ans = []
         nums.sort()
-        if nums[0] > 0 or len(nums) < 3: return result # bad cases
-        
+
         for i, val in enumerate(nums):
-            if i > 0 and nums[i] == nums[i - 1]: continue  # ignore any duplicates
+            if val > 0:  # bad case
+                break
+            if i > 0 and val == nums[i - 1]:  # ignore any duplicates
+                continue
+
             left, right = i + 1, len(nums) - 1
-            
             while left < right:
-                curSum = val + nums[left] + nums[right]
-                if curSum > 0:
+                cur_sum = nums[left] + val + nums[right]
+                if cur_sum > 0:
                     right -= 1
-                elif curSum < 0:
+                elif cur_sum < 0:
                     left += 1
                 else:
-                    result.append([val, nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:  # ignore any duplicates
+                    ans.append([val, nums[left], nums[right]])
+                    left += 1  # move the pointer inward
+                    right -= 1  # move the pointer inward
+                    while nums[left] == nums[left - 1] and left <right:  # ignore any duplicates
                         left += 1
-                    while left < right and nums[right] == nums[right - 1]:  # ignore any duplicates
-                        right -= 1
-                    left += 1  # increment the left pointer
-                    right -= 1  # decrement the right pointer
-        
-        return result
+            
+        return ans
